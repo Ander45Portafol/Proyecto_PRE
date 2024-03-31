cccb7d81eb684eeda2e63dd52e1616c126d1df14
Fernando Gamero <gamero12jamorant@gmail.com>
2024-03-31 15:57:18 -0600

    Cambio de colores

--- Cajero.Designer.cs
+++ Cajero.Designer.cs
@@ -52,6 +52,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(209, 28);
             this.label1.Name = "label1";
@@ -240,7 +241,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(705, 474);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
